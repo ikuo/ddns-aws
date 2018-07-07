c072eb7ddc0fb8bb22eb5312b7f46ec4384c538a
Ikuo Matsumura <ikuo@initials.im>
2018-07-07 22:35:29 +0900

    Setup configuration

--- chkip/Program.fs
+++ chkip/Program.fs
@@ -1,6 +1,6 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
+open System.IO
+open Microsoft.Extensions.Configuration
 open Hopac
 open HttpFs.Client
 
@@ -8,7 +8,14 @@ let ipAddress (): String =
   let body = Request.createUrl Get "http://checkip.amazonaws.com" |> Request.responseAsString |> run
   body.Trim()
 
+let configuration () =
+  let path = Directory.GetCurrentDirectory()
+  (new ConfigurationBuilder()).AddJsonFile(Path.Combine[| path; "config.json" |])
+    .Build()
+
 [<EntryPoint>]
 let main argv =
+  let config = configuration()
+  printfn "ZoneID = %s" (config.["ZoneID"])
   printfn "ip = %s" (ipAddress())
   0
