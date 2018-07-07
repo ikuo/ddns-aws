3cacc3b34f01469bc42371f2bb6d29b7bd0fd7ae
Ikuo Matsumura <ikuo@initials.im>
2018-07-07 21:37:08 +0900

    Call chkip API

--- chkip/Program.fs
+++ chkip/Program.fs
@@ -1,8 +1,14 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Hopac
+open HttpFs.Client
+
+let ipAddress (): String =
+  let body = Request.createUrl Get "http://checkip.amazonaws.com" |> Request.responseAsString |> run
+  body.Trim()
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!!"
-    0 // return an integer exit code
+  printfn "ip = %s" (ipAddress())
+  0
