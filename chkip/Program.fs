42d012805dac315187a4b2e792ae4ecddb0643f5
Ikuo Matsumura <ikuo@initials.im>
2018-07-08 10:08:43 +0900

    Add DNS lookup

--- chkip/Program.fs
+++ chkip/Program.fs
@@ -1,21 +1,34 @@
 ﻿open System
 open System.IO
+open System.Linq
 open Microsoft.Extensions.Configuration
 open Hopac
 open HttpFs.Client
+open DnsClient
 
-let ipAddress (): String =
-  let body = Request.createUrl Get "http://checkip.amazonaws.com" |> Request.responseAsString |> run
-  body.Trim()
-
-let configuration () =
+let configuration (): IConfigurationRoot =
   let path = Directory.GetCurrentDirectory()
   (new ConfigurationBuilder()).AddJsonFile(Path.Combine[| path; "config.json" |])
     .Build()
 
+let ipOfMe (): String =
+  let body = Request.createUrl Get "http://checkip.amazonaws.com" |> Request.responseAsString |> run
+  body.Trim()
+
+let ipByDns (fqdn: String): String =
+  let client = new LookupClient()
+  client.UseCache <- false
+  client.Query(fqdn, QueryType.A).Answers.ARecords().FirstOrDefault().Address.ToString()
+
 [<EntryPoint>]
 let main argv =
   let config = configuration()
+  let fqdn: String = config.["FQDN"]
+  let ip = ipOfMe()
+  let ipDns = ipByDns(fqdn)
   printfn "ZoneID = %s" (config.["ZoneID"])
-  printfn "ip = %s" (ipAddress())
+  if ip.Equals(ipDns) then
+    printfn "Skipping because %s already points to %s" fqdn ip
+  else
+    printfn "Updating %s; %s (old) --to--> %s (new)" fqdn ipDns ip
   0
